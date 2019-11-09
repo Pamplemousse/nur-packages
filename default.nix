9b8848d63a600b98c7d71daf05d5ddfe3ac036a3
Pamplemousse <xav.maso@gmail.com>
2019-11-09 11:04:17 -0700

    init python3Packages.crypto-commons: unstable-2019-11-09

--- default.nix
+++ default.nix
@@ -8,13 +8,13 @@
 
 { pkgs ? import <nixpkgs> { } }:
 
-{
+rec {
   # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+  lib = import ./lib { inherit pkgs; };
+  modules = import ./modules;
+  overlays = import ./overlays;
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  python3Packages = {
+    crypto-commons = pkgs.python3.pkgs.callPackage ./pkgs/python-modules/crypto-commons {};
+  };
 }
